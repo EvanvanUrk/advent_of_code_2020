86529a4e289b49dcb6e91b0d94b2c04473034d04
Evan van Urk <evanvurk@gmail.com>
2020-12-11 13:19:58 +0100

    Finish day_10

--- src/bin/day_10.rs
+++ src/bin/day_10.rs
@@ -3,7 +3,7 @@ extern crate advent_of_code_2020;
 use self::advent_of_code_2020::*;
 
 fn main() {
-    let input = get_input("day_10_test_2.txt");
+    let input = get_input("day_10.txt");
 
     let mut joltages: Vec<u32> = input.split('\n')
         .filter(|line| line.len() > 0)
@@ -28,33 +28,30 @@ fn main() {
     for j in &joltages[1..] {
         if *j == prev + 1 {
             contiguous += 1;
-        } else if contiguous > 2 {
+        } else if contiguous > 1 {
             contiguous_lens.push(contiguous);
+            contiguous = 0;
+        } else {
             contiguous = 0;
         }
         prev = *j;
     }
 
-    println!("{:?}", contiguous_lens);
+    let part_2: u64 = contiguous_lens.iter()
+        .map(|&x| tribonacci(x) as u64)
+        .product();
 
-    let part_2: Vec<u32> = contiguous_lens.iter()
-        .map(|&x| 3u32.pow(x - 3) + 1)
-        .collect();
+    println!("{}", part_2);
+}
 
-    println!("{:?}", part_2);
-    println!("{}", part_2.iter().product::<u32>());
-
-    // joltages.reverse();
-
-    // let combinations: usize = joltages.iter()
-    //     .map(|&x| {
-    //         joltages.iter()
-    //             .filter(|&y| *y > x)
-    //             .filter(|&y| *y - x <= 3)
-    //             .count()
-    //     })
-    //     .filter(|&x| x > 1)
-    //     .sum();
-
-    // println!("{:?}", combinations);
+fn tribonacci(x: u32) -> u32 {
+    match x {
+        1 => 1,
+        2 => 2,
+        3 => 4,
+        4 => 7,
+        5 => 13,
+        6 => 24,
+        _ => panic!("No tribonacci number entered for x = {}", )
+    }
 }
